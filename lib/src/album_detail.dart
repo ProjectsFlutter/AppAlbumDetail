10c891179eedf25f2233fd358489df725955f01e
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-09-24 17:42:56 -0500

    Floating button, header and album description

--- lib/src/album_detail.dart
+++ lib/src/album_detail.dart
@@ -1,3 +1,4 @@
+import 'package:app_album_details/src/album_info.dart';
 import 'package:flutter/material.dart';
 
 class AlbumDetail extends StatelessWidget {
@@ -6,18 +7,59 @@ class AlbumDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        leading: BackButton(
-          onPressed: (){},
-          color: Colors.black,
-        ),
-        backgroundColor: Colors.transparent
+      backgroundColor: Colors.white,
+      body: CustomScrollView(
+        slivers: [
+          SliverPersistentHeader(
+            delegate: _AlbumHeader(),
+            pinned: true,
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+              child: Text(album.description, 
+              textAlign: TextAlign.left,
+              style: const TextStyle(
+                fontWeight: FontWeight.w600,
+                letterSpacing: -0.5,
+                height: 1.5,
+                color: Colors.grey,
+                fontSize: 16.0
+              ),
+              ),
+            ),
+          )
+        ],
       ),
-      body: Center(
-        child: Container(
-        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: Colors.black,
+        child: const Icon(Icons.add_outlined),
       ),
     );
   }
+}
+
+class _AlbumHeader extends SliverPersistentHeaderDelegate{
+  final double _maxExtent = 350;
+  final double _minExtent = 100;
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    // TODO: implement build
+    return Container(
+      color: const Color(0xFFECECEA),
+    );
+  }
+
+  @override
+  // TODO: implement maxExtent
+  double get maxExtent => _maxExtent;
+
+  @override
+  // TODO: implement minExtent
+  double get minExtent => _minExtent;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) => false;
+
 }
