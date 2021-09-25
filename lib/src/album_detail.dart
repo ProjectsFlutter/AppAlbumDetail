ff323fe6572767cc6848f9785f98d495a4334d81
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-09-24 19:46:23 -0500

    Image animation and size adjustments

--- lib/src/album_detail.dart
+++ lib/src/album_detail.dart
@@ -1,5 +1,6 @@
 import 'package:app_album_details/src/album_info.dart';
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 class AlbumDetail extends StatelessWidget {
   const AlbumDetail({Key? key}) : super(key: key);
@@ -41,26 +42,48 @@ class AlbumDetail extends StatelessWidget {
 }
 
 class _AlbumHeader extends SliverPersistentHeaderDelegate{
-  final double _maxExtent = 350;
-  final double _minExtent = 100;
+  final _maxHeader = 350.0;
+  final _minHeader = 100.0;
   
   @override
   Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
     // TODO: implement build
+    final _size = MediaQuery.of(context).size;
+
+    const _maxImage = 180.0;
+    const _minImage = 80.0;
+
+    const _maxLeftDisc = 150.0;
+    const _minLeftDisc = 32.0;
+
+    const _maxArtistName = 30.0;
+    const _minArtistName = 20.0;
+
+    const _maxAlbumName = 25.0;
+    const _minAlbumName = 15.0;
+
+    final _percent = shrinkOffset / _maxHeader;
+
+    final _sizeImage = (_maxImage * (1 - _percent)).clamp(_minImage, _maxImage);
+    final _sizeLeftDisc = (_maxLeftDisc * (1 - _percent)).clamp(_minLeftDisc, _maxLeftDisc);
+
     return Container(
       color: const Color(0xFFECECEA),
       child: Stack(
         children: [
           Positioned(
-            right: 30.0,
-            bottom: 30.0,
-            height: 180.0,
-            child: Image.asset(album.imageDisc),
+            left: _sizeLeftDisc,
+            bottom: _size.height * 0.05,
+            height: _sizeImage,
+            child: Transform.rotate(
+              angle: vector.radians(360 * _percent),
+              child: Image.asset(album.imageDisc)
+            ),
           ),
           Positioned(
-            left: 30.0,
-            bottom: 30.0,
-            height: 180.0,
+            left: _size.height * 0.05,
+            bottom: _size.height * 0.05,
+            height: _sizeImage,
             child: Image.asset(album.imageAlbum),
           ),
           Positioned(
@@ -69,8 +92,8 @@ class _AlbumHeader extends SliverPersistentHeaderDelegate{
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(album.artist, style: const TextStyle(color: Colors.black, fontSize: 30, fontWeight: FontWeight.bold)),
-                Text(album.album,  style: const TextStyle(color: Colors.grey,  fontSize: 25, fontWeight: FontWeight.w400)),
+                Text(album.artist, style: const TextStyle(color: Colors.black, fontSize: _maxArtistName, fontWeight: FontWeight.bold)),
+                Text(album.album,  style: const TextStyle(color: Colors.grey,  fontSize: _maxAlbumName, fontWeight: FontWeight.w400)),
               ],
             ),
           ),
@@ -81,11 +104,11 @@ class _AlbumHeader extends SliverPersistentHeaderDelegate{
 
   @override
   // TODO: implement maxExtent
-  double get maxExtent => _maxExtent;
+  double get maxExtent => _maxHeader;
 
   @override
   // TODO: implement minExtent
-  double get minExtent => _minExtent;
+  double get minExtent => _minHeader;
 
   @override
   bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) => false;
